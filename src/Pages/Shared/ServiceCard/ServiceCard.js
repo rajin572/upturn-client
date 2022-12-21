358415adb6dd6c5aeb7f9dc51d56f2345bf71139
Din Islam Rajin <dirrazin572@gmail.com>
2022-12-21 22:03:18 +0600

    adde the button in the bottom possition

--- src/Pages/Shared/ServiceCard/ServiceCard.js
+++ src/Pages/Shared/ServiceCard/ServiceCard.js
@@ -3,18 +3,20 @@ import "react-photo-view/dist/react-photo-view.css";
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import './ServiceCard.css'
 
 const ServiceCard = ({ service }) => {
   console.log(service);
   const { _id, img, description, price, title } = service;
   return (
-    <Card>
+    <Card className="">
       <PhotoProvider>
         <PhotoView src={img}>
           <Card.Img variant="top" src={img} />
         </PhotoView>
       </PhotoProvider>
-      <Card.Body className="px-1">
+      <Card.Body className="px-1 card-posstion">
+        <div className="mb-5">
         <Card.Title>
           {" "}
           <h3>{title}</h3>{" "}
@@ -23,11 +25,14 @@ const ServiceCard = ({ service }) => {
           {description.slice(0, 100)}...
         </Card.Text>
         <h3>Price: ${price}</h3>
+        </div>
+        <div className="button-possition w-100">
         <Link to={`/services/${_id}`}>
-          <Button variant="primary" className="w-100 mt-3">
+          <Button variant="primary" className="d-block w-100">
             View Details
           </Button>
         </Link>
+        </div>
       </Card.Body>
     </Card>
   );
