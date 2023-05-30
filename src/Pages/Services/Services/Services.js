6ccf6d36e03905bbd924f6c5bc6432bc5c47da24
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-05-30 19:19:11 +0600

    t

--- src/Pages/Services/Services/Services.js
+++ src/Pages/Services/Services/Services.js
@@ -6,8 +6,7 @@ import './Services.css'
 
 const Services = () => {
     useTitle('services')
-    const data = useLoaderData()
-    console.log(data);
+
     return (
         <div className='serviceSection'>
             <h1 className='text-center'>Services</h1>
